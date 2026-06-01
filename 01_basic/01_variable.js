3a4994054e1699cbbbdaeff0cfbbf325fd9174ae
Rupesh <213110726+Jr-R-007@users.noreply.github.com>
2026-06-01 17:35:16 +0000

    new file:   01_basic/01_variable.js

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+accountid = 13932
+accountEmail = "new@gmail.com"
+accountPassword = "12345"
+accountcity = "Mumbai"  
+
+
+
+//accountid = 23244
+accountEmail = "old@gmail.com"
+accountPassword = "67890"
+accountcity = "Bengluru"
+
+console.log(accountid);
+
+console.table([accountid,accountEmail,accountPassword,accountcity]);
